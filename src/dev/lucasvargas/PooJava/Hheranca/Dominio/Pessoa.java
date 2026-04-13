87e8328618dec7a3abfabca47f95f380bfa0e645
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-04-13 16:13:08 -0300

    EEEstudos POO - H erança pt 01

---
+++ src/dev/lucasvargas/PooJava/Hheranca/Dominio/Pessoa.java
@@ -0,0 +1,37 @@
+package dev.lucasvargas.PooJava.Hheranca.Dominio;
+
+public class Pessoa {
+    private String nome;
+    private String cfp;
+    private Endereco endereco;
+
+    public void imprime(){
+        System.out.println("Nome: " + this.nome);
+        System.out.println("CPF: " + this.cfp);
+        System.out.println("Endereço: " + endereco.getRua() + " Cep: " + endereco.getCep());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCfp() {
+        return cfp;
+    }
+
+    public void setCfp(String cfp) {
+        this.cfp = cfp;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
---
+++ src/dev/lucasvargas/PooJava/Hheranca/Test/HerancaTest01.java
@@ -0,0 +1,38 @@
+package dev.lucasvargas.PooJava.Hheranca.Test;
+
+import dev.lucasvargas.PooJava.Hheranca.Dominio.Endereco;
+import dev.lucasvargas.PooJava.Hheranca.Dominio.Funcionario;
+import dev.lucasvargas.PooJava.Hheranca.Dominio.Pessoa;
+
+import java.util.Scanner;
+
+public class HerancaTest01 {
+
+    static void main() {
+
+        Scanner scn = new Scanner(System.in);
+
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua Itapage");
+        endereco.setCep("24722-180");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("LUCAS");
+        pessoa.setCfp("999.999.999-200");
+        pessoa.setEndereco(endereco);
+
+        System.out.println("===== PESSOA =====");
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("João");
+        funcionario.setCfp("111.222.333-44");
+        funcionario.setSalario(1600);
+        funcionario.setEndereco(endereco);
+
+        System.out.println("===== FUNCIONARIO =====");
+        funcionario.imprime();
+
+
+    }
+}
