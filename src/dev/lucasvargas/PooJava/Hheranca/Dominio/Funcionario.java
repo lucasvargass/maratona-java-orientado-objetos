3e6a590ab2a96d057237ec773a3134f8cc93e151
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-04-13 18:39:10 -0300

    Estudos POO - Herança pt 02 - Super

--- src/dev/lucasvargas/PooJava/Hheranca/Dominio/Funcionario.java
+++ src/dev/lucasvargas/PooJava/Hheranca/Dominio/Funcionario.java
@@ -3,6 +3,11 @@ package dev.lucasvargas.PooJava.Hheranca.Dominio;
 public class Funcionario extends Pessoa{
     private double salario;
 
+    public void imprime(){
+        super.imprime();
+        System.out.println("Salario: " + this.salario);
+    }
+
     public double getSalario() {
         return salario;
     }
