86ee205ac9952b568489abaa4258349b208e20b0
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-04-16 10:07:04 -0300

    Estudos POO - Sobrescrita do método toString

---
+++ src/dev/lucasvargas/PooJava/Isobrescrita/Test/AnimeTest01.java
@@ -0,0 +1,13 @@
+package dev.lucasvargas.PooJava.Isobrescrita.Test;
+
+import dev.lucasvargas.PooJava.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    static void main() {
+
+        Anime anime = new Anime("LUCAS VARGAS");
+        System.out.println(anime);
+
+
+    }
+}
