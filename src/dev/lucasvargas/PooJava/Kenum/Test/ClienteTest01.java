4bfc5321001b154e7ceade13fe51c491fefd8c57
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-04-17 18:16:43 -0300

    Estudos POO - Enumeração pt 02 - Construtores e atributos

--- src/dev/lucasvargas/PooJava/Kenum/Test/ClienteTest01.java
+++ src/dev/lucasvargas/PooJava/Kenum/Test/ClienteTest01.java
@@ -6,15 +6,11 @@ import dev.lucasvargas.PooJava.Kenum.dominio.TipoCliente;
 public class ClienteTest01 {
     static void main() {
 
-        Cliente cliente1 = new Cliente("Lucas", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("João", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente3 = new Cliente("Fernando", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Luiz", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Lucas", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente2 = new Cliente("João", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.DEBITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
 
 
 
--- src/dev/lucasvargas/PooJava/Kenum/dominio/TipoCliente.java
+++ src/dev/lucasvargas/PooJava/Kenum/dominio/TipoCliente.java
@@ -1,6 +1,18 @@
 package dev.lucasvargas.PooJava.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Fisica"),
+    PESSOA_JURIDICA(2, "Pessoa Juridica");
+
+    public final int VALOR;
+    private String nomeRelatorio;
+
+    TipoCliente(int VALOR, String nomeRelatorio) {
+        this.VALOR = VALOR;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public String getNomeRelatorio(){
+        return this.nomeRelatorio = nomeRelatorio;
+    }
 }
